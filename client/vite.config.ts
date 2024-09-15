4ad495aeb5007709b3da9fd1ec8cca5d84efe41a
刘刚 <liugang59@meituan.com>
2024-09-16 02:41:34 +0800

    update

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -64,8 +64,8 @@ export default defineConfig({
         maximumFileSizeToCacheInBytes: 3 * 1024 * 1024,
       },
       manifest: {
-        name: 'LibreChat',
-        short_name: 'LibreChat',
+        name: 'DabaoChat',
+        short_name: 'DabaoChat',
         start_url: '/',
         display: 'standalone',
         background_color: '#000000',
